bb809928fef08404c6dad5b6bac6cd43bc8f01d8
Seth <teshst>
2024-03-13 21:31:32 -0600

    moved rm commands to top of setup script

--- .util/setup_bare_repo.sh
+++ .util/setup_bare_repo.sh
@@ -1,17 +1,17 @@
 !#/bin/bash
 
-# Clone Git Repo
-git clone --bare git@github.com:teshst/dotfiles.git $HOME/.cfg
-
-# Temp Alias
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-
 # RM Existing Files
 rm $HOME/.bashrc
 rm $HOME/.zshrc
 rm -rf $HOME/.config/nvm
 rm -rf $HOME/.config/kitty
 rm -rf $HOME/.config/zsh
+
+# Clone Git Repo
+git clone --bare git@github.com:teshst/dotfiles.git $HOME/.cfg
+
+# Temp Alias
+alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 # Load Files
 config checkout
